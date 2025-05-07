0d28746add3e842b6ce1a111c9cad74ab7120fdc
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-05-07 22:00:00 +0530

    subarraysum brute force

--- Array/maxSubarrSum.cpp
+++ Array/maxSubarrSum.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 // TC: O(n^2)
-int brute_force(int arr, int n) {
-    int sum=0;
+int brute_force(int arr[], int n) {
+    int maxSum = INT_MIN;
     for (int st=0; st<n; st++) {
+        int currSum=0;
         for (int end=st; end<n; end++) {
-            sum += end;
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
         }
     }
-    return sum;
+    return maxSum;
 }
 
 // TC: O(n)
-int kadane_algo(int arr, int n) {
+int kadane_algo(int arr[], int n) {
     return 0;
 }
 
@@ -30,8 +32,8 @@ int main() {
         cin >> arr[i];
     }
 
-    printf("Brute force result: %d", brute_force(*arr, size));
-    printf("Kadane algo result: %d", kadane_algo(*arr, size));
+    printf("Brute force result: %d", brute_force(arr, size));
+    printf("Kadane algo result: %d", kadane_algo(arr, size));
 
     return 0;
 }
