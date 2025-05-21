f3791c574e379817a5fd4df2c46481fcbb9d598c
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-05-21 22:26:05 +0530

    updated files

--- CSES/Introductory/weirdAlgo.cpp
+++ CSES/Introductory/weirdAlgo.cpp
@@ -4,6 +4,7 @@ Consider an algorithm that takes as input a positive integer n.
 If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
 The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
 Your task is to simulate the execution of the algorithm for a given value of n.
 
 Input: The only input line contains an integer n.
@@ -38,3 +39,4 @@ int main() {
     
     return 0;
 }
+
